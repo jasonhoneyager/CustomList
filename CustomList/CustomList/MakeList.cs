9e4a82593c34f663ba59469133b8de9d0a7354b6
Jason Honeyager <jasonhoneyager@gmail.com>
2016-06-13 17:01:18 -0500

    updating initial generic list class project

---
+++ CustomList/CustomList/MakeList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    class MakeList<T>
+    {
+        public T[] collection = new T[10];
+        int index = 0;
+
+        public MakeList()
+        {
+
+        }   
+
+        public void AddItem(T listitem)
+        {
+            collection[index] = listitem;
+            index++;
+        }
+
+        public void ViewList()
+        {
+            Console.WriteLine("[{0}]", string.Join(", ", collection));
+        }
+    }
+}
+
---
+++ CustomList/CustomList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            MakeList<string> makelist = new MakeList<string>();
+            while (loop == true)
+            {
+                Console.WriteLine("Enter List Variable");
+                var listitem = Console.ReadLine();
+                string listitem2 = Convert.ToString(listitem);
+                makelist.AddItem(listitem2);
+                makelist.ViewList();
+                Console.ReadKey();
+            }
+        }
+    }
+}
